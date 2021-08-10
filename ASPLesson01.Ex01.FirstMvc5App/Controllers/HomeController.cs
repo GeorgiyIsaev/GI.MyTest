b6e3e2976b46650ec753d7330b0bec00d4c8bb6d
Георгий Исаев <georgiyelbaf@gmail.com>
2021-08-10 21:50:29 +0300

    Контролы

--- ASPLesson01.Ex01.FirstMvc5App/Controllers/HomeController.cs
+++ ASPLesson01.Ex01.FirstMvc5App/Controllers/HomeController.cs
@@ -3,28 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ASPLesson01.Ex01.FirstMvc5App.Models;
 
 namespace ASPLesson01.Ex01.FirstMvc5App.Controllers
 {
     public class HomeController : Controller
     {
+        // создаем контекст данных
+        BookContext db = new BookContext();
+
         public ActionResult Index()
         {
+            // получаем из бд все объекты Book
+            IEnumerable<Book> books = db.Books;
+            // передаем все объекты в динамическое свойство Books в ViewBag
+            ViewBag.Books = books;
+            // возвращаем представление
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+       
     }
 }
