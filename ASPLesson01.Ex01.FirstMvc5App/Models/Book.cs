6b020bc8633ebe1b6e502081dcffd1730407d7f0
Георгий Исаев <georgiyelbaf@gmail.com>
2021-08-10 21:23:26 +0300

    Add class Book and Purchase

---
+++ ASPLesson01.Ex01.FirstMvc5App/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPLesson01.Ex01.FirstMvc5App.Models
+{
+    public class Book
+    {   
+        // ID книги
+        public int Id { get; set; }
+        // название книги
+        public string Name { get; set; }
+        // автор книги
+        public string Author { get; set; }
+        // цена
+        public int Price { get; set; }
+    }
+}
